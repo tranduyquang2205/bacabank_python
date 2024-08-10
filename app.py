6e83b968715bf924477cbaec297914d19da6a057
tranduyquang <tranduyquang2205@gmail.com>
2024-08-10 17:06:27 +0700

    update

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 from bab import BacABank
-
+import sys
+import traceback
+from api_response import APIResponse
 
 app = FastAPI()
 @app.get("/")
@@ -15,15 +17,25 @@ class LoginDetails(BaseModel):
     account_number: str
 @app.post('/login', tags=["login"])
 def login_api(input: LoginDetails):
-        
+    try:
         session_raw = bacabank.login(input.username, input.password)
-        return session_raw
-
+        return APIResponse.json_format(session_raw)
+    except Exception as e:
+        response = str(e)
+        print(traceback.format_exc())
+        print(sys.exc_info()[2])
+        return APIResponse.json_format(response)  
 @app.post('/get_balance', tags=["get_balance"])
 def get_balance_api(input: LoginDetails):
+    try:
         session_raw = bacabank.login(input.username, input.password)
         balance = bacabank.get_balance()
-        return balance
+        return APIResponse.json_format(balance)
+    except Exception as e:
+        response = str(e)
+        print(traceback.format_exc())
+        print(sys.exc_info()[2])
+        return APIResponse.json_format(response)  
 class Transactions(BaseModel):
     username: str
     password: str
@@ -34,8 +46,14 @@ class Transactions(BaseModel):
     
 @app.post('/get_transactions', tags=["get_transactions"])
 def get_transactions_api(input: Transactions):
+    try:
         session_raw = bacabank.login(input.username, input.password)
         history = bacabank.get_transactions(input.from_date,input.to_date,input.limit)
-        return history
+        return APIResponse.json_format(history)
+    except Exception as e:
+        response = str(e)
+        print(traceback.format_exc())
+        print(sys.exc_info()[2])
+        return APIResponse.json_format(response)  
 if __name__ == "__main__":
     uvicorn.run(app ,host='0.0.0.0', port=3000)
